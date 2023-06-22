58cdafbaa85fdf758591b94a92488759b8550208
yunsulee <lws8905@gmail.com>
2023-06-22 14:49:00 +0900

    oracle

--- src/main/java/com/example/accessingdatamysql/User.java
+++ src/main/java/com/example/accessingdatamysql/User.java
@@ -4,7 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+
+@Table(name="Users")
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
 	@Id
