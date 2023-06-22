9eea6f9d5a3b998aef844f3cdb571f2521878962
yunsulee <lws8905@gmail.com>
2023-06-22 15:35:12 +0900

    update, delete

--- src/main/java/com/example/accessingdatamysql/MainController.java
+++ src/main/java/com/example/accessingdatamysql/MainController.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,6 +30,26 @@ public class MainController {
 		n.setEmail(email);
 		userRepository.save(n);
 		return "Saved";
+	}
+
+
+	@DeleteMapping (path="/user")
+	public @ResponseBody String deleteUser (@RequestParam Integer id){
+		User n = new User();
+		n.setId(id);
+		userRepository.delete(n);
+		return "Delete";
+	}
+
+	
+	@PutMapping(path="/user")
+	public @ResponseBody String updateUser (@RequestParam Integer id, @RequestParam String name, @RequestParam String email){
+		User n = new User();
+		n.setId(id);
+		n.setName(name);
+		n.setEmail(email);
+		userRepository.save(n);
+		return "Update";
 	}
 
 	@GetMapping(path="/users")
